361823fb3554d3277d57925c706e37f09ef145a8
meamruri <meammf@yandex.ru>
2018-03-09 19:41:15 +0300

    impl Vertex hash

--- graph/graph.go
+++ graph/graph.go
@@ -1,15 +1,47 @@
 package graph
 
-type Weight struct {
-	Min int
-	Max int
-	Delta int
-	Dispersion int
+type Vertex interface {
+	GetHash()		uint64
 }
 
-type Vertex struct {
-	Children map[int]Weight
+type vertex struct {
+	hash			uint64
 }
+
+func (v *vertex) GetHash() uint64 {
+	return v.hash
+}
+
+type Weight interface {
+	GetMin()		int
+	GetMax()		int
+	GetDelta()		int
+	GetDispersion()	int
+}
+
+type weight struct {
+	min			int
+	max			int
+	delta		int
+	dispersion 	int
+}
+
+type Edge interface {
+	Source()		Vertex
+	Target()		Vertex
+	GetWeight()		Weight
+}
+
+type edge struct {
+	source	Vertex
+	target	Vertex
+	wgt		Weight
+}
+
+
+//type Vertex struct {
+//	Children map[int]Weight
+//}
 
 type Graph struct {
 	Root int	// hash of root vertex
