8d8cc0c124efc8367a94b88b02a33a3b0cec934e
meamruri <meammf@yandex.ru>
2018-03-10 12:55:04 +0300

    graph impl

--- graph/graph.go
+++ graph/graph.go
@@ -1,8 +1,32 @@
 package graph
 
-type Graph struct {
-	Root int	// hash of root vertex
-	Current int	// hash of current vertex
-	Vertexes map[int]Vertex
+import (
+	"hash/fnv"
+)
+
+type Graph interface {
+
+}
+
+type graph struct {
+	Root uint64				// hash of root vertex
+	Current uint64			// hash of current vertex
+	Vertexes map[uint64]Vertex
 	Finished bool
+}
+
+func newGraph(body string) *graph {
+	h := fnv.New64()
+	h.Write([]byte(body))
+	sum := h.Sum64()
+	return &graph{
+		Root: sum,
+		Current: sum,
+		Finished: false,
+		Vertexes: make(map[uint64]Vertex),
+	}
+}
+
+func NewGraph(body string) Graph {
+	return newGraph(body)
 }
